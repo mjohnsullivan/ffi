71b77055e2a7ff18d306190ad14b0fa8597da939
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-08-02 08:53:24 -0700

    Added more doc comments

--- primitives/primitives.dart
+++ primitives/primitives.dart
@@ -1,15 +1,18 @@
 import 'dart:ffi' as ffi;
 
 // C sum function - int sum(int a, int b);
+// Example of how to pass paramters into C and use the returned result
 typedef sum_func = ffi.Int32 Function(ffi.Int32 a, ffi.Int32 b);
 typedef Sum = int Function(int a, int b);
 
 // C subtract function - int subtract(int *a, int b);
+// Example of how to create pointers in Dart, alloc them, and pass them as parameters
 typedef subtract_func = ffi.Int32 Function(
     ffi.Pointer<ffi.Int32> a, ffi.Int32 b);
 typedef Subtract = int Function(ffi.Pointer<ffi.Int32> a, int b);
 
 // C multiply function - int *multiply(int a, int b);
+// Example of how to receive pointers in Dart and access the data
 typedef multiply_func = ffi.Pointer<ffi.Int32> Function(
     ffi.Int32 a, ffi.Int32 b);
 typedef Multiply = ffi.Pointer<ffi.Int32> Function(int a, int b);
@@ -23,7 +26,9 @@ main() {
   print('3 + 5 = ${sum(3, 5)}');
 
   // calls int subtract(int *a, int b);
+  // Create a pointer
   final p = ffi.Pointer<ffi.Int32>.allocate();
+  // Place a value into the address
   p.store(3);
   final subtractPointer =
       dylib.lookup<ffi.NativeFunction<subtract_func>>('subtract');
@@ -34,6 +39,8 @@ main() {
   final multiplyPointer =
       dylib.lookup<ffi.NativeFunction<multiply_func>>('multiply');
   final multiply = multiplyPointer.asFunction<Multiply>();
-  final result = multiply(3, 5);
-  print('3 * 5 = ${result.load<int>()}');
+  final resultPointer = multiply(3, 5);
+  // Fetch the result at the address pointed to
+  final result = resultPointer.load<int>();
+  print('3 * 5 = $result');
 }
